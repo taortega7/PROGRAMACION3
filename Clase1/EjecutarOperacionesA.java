31c797fa179e7594c49efede9edd5d15a91ae355
Tati <taortega@estudiante.uniajc.edu.co>
2025-08-12 17:36:10 +0000

    Operaciones

--- Clase1/EjecutarOperacionesA.java
+++ Clase1/EjecutarOperacionesA.java
@@ -11,6 +11,6 @@ public class EjecutarOperacionesA {
  
         objOp.mostrarArregloEnteros(x);
  
- 
+        System.out.println(objOp.);
     }
 }
--- Clase1/OperacionesArreglos.java
+++ Clase1/OperacionesArreglos.java
@@ -1,41 +1,43 @@
 import java.util.Arrays;
-
+ 
 public class OperacionesArreglos {
-
+ 
     /*
      * Método para llenar el arreglo con números enteros y retornarlo
      */
-    public int[] llenarArregloEnteros(int tam) {
-
+    public int[] llenarArregloEnteros(int tam){
+ 
         int[] a = new int[tam];
-        for (int i = 0; i < a.length; i++) {
-            a[i] = (int) (1 + Math.random() * (100 - 1) + 1);
+        for(int i = 0; i < a.length; i++){
+            a[i] = (int) (1 + Math.random() * (100-1) + 1);
         }
-
+ 
         return a;
     }
-
+ 
     /*
      * Método para mostrar el arreglo de números enteros
      */
-
-    public void mostrarArregloEnteros(int[] a) {
+ 
+    public void mostrarArregloEnteros(int[] a){
         System.out.println(Arrays.toString(a));
     }
-
-    public double promedioArregloEnteros (int[] a){
-        int suma = 0; 
-        double promedio = 0.0; 
-
-        for (int i = 0; i < a.length; i++){
+ 
+    /*
+     * Método para calcular el promedio de un arreglo
+     */
+ 
+    public double promedioArregloEnteros(int[] a){
+        int suma = 0;
+        double promedio = 0.0;
+ 
+        for (int i = 0; i < a.length; i++) {
             suma += a[i];
         }
-
-        promedio = suma / a.length; 
-
+ 
+        promedio = suma / a.length;
+ 
         return promedio;
-
-
-
-}
+    }
+ 
 }
