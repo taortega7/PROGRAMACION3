b785b557696956ab10a28ce9a9f3fdfbaef7c181
Tati <taortega@estudiante.uniajc.edu.co>
2025-08-12 17:37:13 +0000

    operaciones

--- Clase1/EjecutarOperacionesA.java
+++ Clase1/EjecutarOperacionesA.java
@@ -1,16 +1,18 @@
 public class EjecutarOperacionesA {
     public static void main(String[] args){
- 
+
         OperacionesArreglos objOp = new OperacionesArreglos();
- 
+
         //Llenar el arreglo
- 
-        int[] x = objOp.llenarArregloEnteros(4);
- 
+
+        int[] x = objOp.llenarArregloEnteros(3);
+
         //Mostrar el arreglo
- 
+
         objOp.mostrarArregloEnteros(x);
- 
-        System.out.println(objOp.);
+
+        System.out.println(objOp.promedioArregloEnteros(x));
+
+
     }
 }
